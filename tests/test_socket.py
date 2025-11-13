ff9d9c45156a1fa224779647d386aca7a66c591f
nivindulakshitha <nivindulakshitha@gmail.com>
2025-11-13 16:32:39 +0530

    Add nosec comment to socket connectivity assertion in test_socket.py

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -8,7 +8,7 @@ async def test_socket():
     sio = socketio.AsyncClient()
     try:
         await sio.connect("http://127.0.0.1:8000", wait_timeout=5, retry=True)
-        assert sio.connected
+        assert sio.connected  # nosec
         await sio.disconnect()
     except Exception as e:
         # If server is not running, skip the test
